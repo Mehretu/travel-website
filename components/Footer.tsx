289cc9097478f68b33181b396748af3eee5f606e
Mehretu <mehertu.abreham@gmail.com>
2024-01-16 11:33:43 +0300

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+
+const Footer = () => {
+  return (
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] ">
+
+        </div>
+
+      </div>
+
+    </footer>
+  )
+}
+
+export default Footer
